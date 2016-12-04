008f9d9cb79438b2d3eac033cc110b3f36bc4e80
AnonimoLopez <jose_lopez_emo@hotmail.com>
2016-12-04 12:44:58 -0600

    bd

--- php/conexion.php
+++ php/conexion.php
@@ -1,10 +1,10 @@
 <?php
 
- $link=mysqli_connect("localhost","root","joselopez156") or die('Error al conectarse a mysql: ' . mysql_error());
+ $link=mysqli_connect("localhost","root","") or die('Error al conectarse a mysql: ' . mysql_error());
 if (phpversion()>6){
   $db_selected = mysqli_select_db($link,"taqueria") or die ('Error al abrir la base de datos: ' . mysql_error());
 }else{
-  $link=mysql_connect("localhost","root","joselopez156") or die('Error al conectarse a mysql: ' . mysql_error());
+  $link=mysql_connect("localhost","root","") or die('Error al conectarse a mysql: ' . mysql_error());
   $db_selected = mysql_select_db("taqueria", $link ) or die ('Error al abrir la base de datos: ' . mysql_error());
 }
 
